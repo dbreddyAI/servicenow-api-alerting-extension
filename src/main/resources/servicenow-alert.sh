2c45f51db97c77cb1824f5c4fe26977ab8f31082
satish.muddam <satish.muddam@appdynamics.com>
2016-02-03 15:57:42 +0530

    Corrected the java path in shell file

--- src/main/resources/servicenow-alert.sh
+++ src/main/resources/servicenow-alert.sh
@@ -1,1 +1,1 @@
-..\..\..\jre\bin\java -Dlog4j.configuration=file:conf/log4j.xml -jar servicenow-api-alert.jar "$@"
+../../../jre/bin/java -Dlog4j.configuration=file:conf/log4j.xml -jar servicenow-api-alert.jar "$@"
