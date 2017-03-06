f292981dfae453ea9c929e56f5c294f1a83f33a3
kunalgupApDx <kunal.gupta@appdynamics.com>
2017-03-06 11:55:25 -0800

    Update servicenow-alert.bat
    Fixing the path issue keeping in mind the 4.3 Platform admin changes.

--- src/main/resources/servicenow-alert.bat
+++ src/main/resources/servicenow-alert.bat
@@ -1,2 +1,2 @@
 @echo off
-..\..\..\jre\bin\java -Dlog4j.configuration=file:conf/log4j.xml -jar servicenow-api-alert.jar %*
+java -Dlog4j.configuration=file:conf/log4j.xml -jar servicenow-api-alert.jar %*
